9bb25104787ff5079094ea250393a80c9e0807a2
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-05-24 16:48:13 +0300

    Add Shortage model; Add fixed calues as Enums (Room, Category).

---
+++ ResourceShortageUI/Models/Shortage.cs
@@ -0,0 +1,13 @@
+﻿using ResourceShortageUI.Enums;
+
+namespace ResourceShortageUI.Models;
+
+public class Shortage
+{
+    public string? Title { get; set; }
+    public string? Name { get; set; }
+    public Room RoomType { get; }
+    public Category ItemCategory { get; }
+    public int Priority { get; set; }
+    public DateTime CreatedOn { get; set; }
+}
